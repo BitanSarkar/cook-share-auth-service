4b10fe90efbac6ba8dc82196c8e685749bc48370
bitanSarkar <bitansarkar12345@gmail.com>
2024-06-14 23:40:40 +0530

    updated service name

--- src/main/java/com/bitsar/passwordlessauth/PasswordLessAuthServiceApplication.java
+++ src/main/java/com/bitsar/passwordlessauth/PasswordLessAuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CookShareAuthServiceApplication {
+public class PasswordLessAuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CookShareAuthServiceApplication.class, args);
+        SpringApplication.run(PasswordLessAuthServiceApplication.class, args);
     }
 
 }
